dc96502693dd8949fa21335921f7c89d5679ea7b
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-10-19 16:34:03 -0600

    Proyecto Final Iniciado

---
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/Backend.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundamentosproyectofinal;
+
+/**
+ *
+ * @author rogerjoseulaterivera
+ */
+public class Backend {
+    
+    // Mensajes
+    public void saludoInicial() {
+        
+        System.out.println("\n");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("|                                         |");
+	System.out.println("|           Inicializando Sistema         |");
+	System.out.println("|                                         |");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("\n");
+        
+    }
+    
+    public void despedida() {
+        
+        System.out.println("\n");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("|                                         |");
+	System.out.println("|           Saliendo del Sistema          |");
+	System.out.println("|                                         |");
+	System.out.println("|              Guardando Datos            |");
+	System.out.println("|                                         |");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("\n");
+        
+    }
+    
+    public void mensajeRegistrarClienteNuevo() {
+        
+        System.out.println("\n");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("|                                         |");
+	System.out.println("|         REGISTRAR CLIENTE NUEVO         |");
+	System.out.println("|                                         |");
+	System.out.println("* --------------------------------------- *");
+	System.out.println("\n");
+        
+    }
+
+    public void mensajeMenuPeliculas() {
+        
+        System.out.println("\n");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("|                                         |");
+        System.out.println("|            MENU DE PELICULAS            |");
+        System.out.println("|                                         |");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("\n");
+                
+    }
+
+    public void mensajeMenuInformacionCliente() {
+        
+        System.out.println("\n");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("|                                         |");
+        System.out.println("|       INFORMACION DE LOS CLIENTES       |");
+        System.out.println("|                                         |");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("\n");
+
+    }
+
+    public void mensajeMenuPrincipal() {
+        
+        System.out.println("\n");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("|                                         |");
+        System.out.println("|              MENU PRINCIPAL             |");
+        System.out.println("|                                         |");
+        System.out.println("* --------------------------------------- *");
+        System.out.println("\n");
+        
+    }
+    
+    
+    
+    
+}
