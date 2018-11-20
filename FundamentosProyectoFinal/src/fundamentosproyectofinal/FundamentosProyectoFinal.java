ad937f992601713778cb0ba99b89915fd571b0df
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-11-20 09:52:56 -0600

    Otras cosas

--- FundamentosProyectoFinal/src/fundamentosproyectofinal/FundamentosProyectoFinal.java
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/FundamentosProyectoFinal.java
@@ -17,25 +17,7 @@ public class FundamentosProyectoFinal {
     
     public static void main(String[] args) {
 
-        // Menus menus = new Menus();
-        InteraccionConCSV backendCSV = new InteraccionConCSV();
-
-        String [] actualizarUsuario = {"1", "Monster", "Ducati", "D", "12345678", "m", "afsdgsdfg", "dsfgdfgdfg", "dfgdfg", "fs", "1", "roger@ducati.com", "true", "false"};
         
-        /*
-        String [] array = null;
-        
-        array = backendCSV.leerCSV("9044526861");
-        
-        System.out.println("Imprimiendo arreglo obtenido");
-        backendCSV.imprimirArreglo(array);
-        */
-        
-        // menus.inicializador();
-        
-        backendCSV.guardarDatosUsuario(actualizarUsuario,"1");
-        
-
         
     }
 
--- FundamentosProyectoFinal/src/fundamentosproyectofinal/Pelicula.java
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/Pelicula.java
@@ -5,87 +5,148 @@
  */
 package fundamentosproyectofinal;
 
+import java.util.InputMismatchException;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author Karla
  */
 public class Pelicula {
-    
-    private int idPelicula;
+
+    // Se crea la clase pelìculas con los atributos necesarios tanto para poder 
+    // crear peliculas nuevas mediante el método correspondiente como para poder buscarla 
+    //dentro del método de consulta de películas. 
     private String nombrePelicula;
+    private int idPelicula;
     private int year;
-    private String descripcion;
     private String director;
-    private int tipoDisco;
-    private int estadoPelicula;
+    private String sinopsis;
+    private String generoPelicula;
+
+    public Pelicula(String nombrePelicula, int idPelicula, int year, String director, String sinopsis, String generoPelicula) {
+        this.nombrePelicula = nombrePelicula;
+        this.idPelicula = idPelicula;
+        this.year = year;
+        this.director = director;
+        this.sinopsis = sinopsis;
+        this.generoPelicula = generoPelicula;
+    }
 
     public Pelicula() {
-    }
-
-    public Pelicula(int idPelicula, String nombrePelicula, int year, String descripcion, String director, int tipoDisco, int estadoPelicula) {
-        this.idPelicula = idPelicula;
-        this.nombrePelicula = nombrePelicula;
-        this.year = year;
-        this.descripcion = descripcion;
-        this.director = director;
-        this.tipoDisco = tipoDisco;
-        this.estadoPelicula = estadoPelicula;
-    }
-
-    public void setIdPelicula(int idPelicula) {
-        this.idPelicula = idPelicula;
-    }
-
-    public void setNombrePelicula(String nombrePelicula) {
-        this.nombrePelicula = nombrePelicula;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public void setTipoDisco(int tipoDisco) {
-        this.tipoDisco = tipoDisco;
-    }
-
-    public void setEstadoPelicula(int estadoPelicula) {
-        this.estadoPelicula = estadoPelicula;
-    }
-
-    public int getIdPelicula() {
-        return idPelicula;
     }
 
     public String getNombrePelicula() {
         return nombrePelicula;
     }
 
+    public void setNombrePelicula(String nombrePelicula) {
+
+        this.nombrePelicula = nombrePelicula;
+    }
+
+    public int getIdPelicula() {
+        return idPelicula;
+    }
+
+    public void setIdPelicula(int idPelicula) {
+        this.idPelicula = idPelicula;
+    }
+
     public int getYear() {
+
         return year;
     }
 
-    public String getDescripcion() {
-        return descripcion;
+    public void setYear() {
+        boolean done = false;
+        do {
+            try {
+                String texto = JOptionPane.showInputDialog("Ingrese el año de la película");
+                this.year = Integer.parseInt(texto);
+                JOptionPane.showMessageDialog(null,"El año ingresado es: "+ this.year);
+                done = true;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null,"Error al ingresar el año, solo escriba números, no letras.");
+            }
+        } while (!done);
+       
     }
 
-    public String getDirector() {
+
+public String getDirector() {
         return director;
     }
-
-    public int getTipoDisco() {
-        return tipoDisco;
+    public void setDirector() {
+        
+         this.director = JOptionPane.showInputDialog("Ingrese el nombre del director.");
+        while (this.director.contains("1") || this.director.contains("2") 
+                || this.director.contains("3")|| this.director.contains("4")
+                || this.director.contains("5")|| this.director.contains("6")
+                || this.director.contains("7")|| this.director.contains("8")
+                || this.director.contains("9")|| this.director.contains("0")) {
+             JOptionPane.showMessageDialog(null, "El nombre no debe contener números. "
+                       + "Ingréselo de nuevo.");
+             this.director = JOptionPane.showInputDialog("Ingrese el nombre del director.");
+        }
+        JOptionPane.showMessageDialog(null,"El nombre ingresado fue: " +this.director);
     }
 
-    public int getEstadoPelicula() {
-        return estadoPelicula;
+    public String getSinopsis() {
+        return sinopsis;
     }
+
+    public void setSinopsis( ) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getGeneroPelicula() {
+        return generoPelicula;
+    }
+
+    public void setGeneroPelicula() {
+        
+         int opcion=Integer.parseInt (JOptionPane.showInputDialog("Ingrese el número correspondiente al género"
+                 + " de la película: "
+                + " \n 1. Comedia. \n 2. Romance \n 3. Drama. \n 4. Scifi. \n 5. Terror."
+                + "\n 6. Fantasía. \n 7. Independiente."));
+                switch (opcion){
+                    case 1:
+                        this.generoPelicula= "Comedia";
+                        JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+                    case 2:
+                        this.generoPelicula= "Romance";
+                        JOptionPane.showMessageDialog(null, "El género elegido  fue: " + this.generoPelicula);
+                        break;
+                        
+                        case 3:
+                        this.generoPelicula= "Drama";
+                        JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+
+                        case 4:
+                        this.generoPelicula= "Scifi";
+                        JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+                        
+                        case 5:
+                        this.generoPelicula= "Terror";
+                        JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+
+                        case 6:
+                        this.generoPelicula= "Fantasía";
+                       JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+
+                        case 7:
+                        this.generoPelicula= "Independiente";
+                        JOptionPane.showMessageDialog(null, "El género elegido fue: " + this.generoPelicula);
+                        break;
+                
+                }
+    }
+    
+ 
 
 }
