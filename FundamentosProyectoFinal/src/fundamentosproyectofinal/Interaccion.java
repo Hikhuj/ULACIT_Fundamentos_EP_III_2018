961feeccc1e3ef4081dd499fbc89c91ca630c9c0
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-11-22 19:19:20 -0600

    Nada

--- FundamentosProyectoFinal/src/fundamentosproyectofinal/Interaccion.java
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/Interaccion.java
@@ -15,15 +15,24 @@ public class Interaccion {
     
     public String [] registrarUsuario() {
         
+        InteraccionConCSV interaccionCSV = new InteraccionConCSV();
+        
         // Arreglo de la informacion nueva que se recolectara
         String [] informacionUsuarioNuevo = new String[14];
         
-        informacionUsuarioNuevo[0] = "Hola";
-        informacionUsuarioNuevo[1] = ;
-        informacionUsuarioNuevo[2] = ;
-        informacionUsuarioNuevo[3] = ;
-        informacionUsuarioNuevo[4] = ;
-        informacionUsuarioNuevo[5] = ;
+        // Usuario nuevo: Id
+        int idUsuarioNuevo = interaccionCSV.setIdNuevo();
+        informacionUsuarioNuevo[0] = String.valueOf(idUsuarioNuevo);
+        // Usuario nuevo: Nombre
+        informacionUsuarioNuevo[1] = JOptionPane.showInputDialog("Ingrese apellido de usuario nuevo");
+        // Usuario nuevo: Apellido
+        informacionUsuarioNuevo[2] = JOptionPane.showInputDialog("Ingrese Segundo Nombre de usuario nuevo");
+        // Usuario nuevo: Segundo Nombre
+        informacionUsuarioNuevo[3] = JOptionPane.showInputDialog("Ingrese nombre de usuario nuevo");
+        // Usuario nuevo: numero Telefonico
+        int telefonoUsuarioNuevo = Integer.parseInt(JOptionPane.showInputDialog("Ingrese telefono nuevo"));
+        informacionUsuarioNuevo[4] = String.valueOf(telefonoUsuarioNuevo);
+        informacionUsuarioNuevo[5] = JOptionPane.showInputDialog("ingrese genero");
         informacionUsuarioNuevo[6] = ;
         informacionUsuarioNuevo[7] = ;
         informacionUsuarioNuevo[8] = ;
@@ -31,8 +40,8 @@ public class Interaccion {
         informacionUsuarioNuevo[10] = ;
         informacionUsuarioNuevo[11] = ;
         informacionUsuarioNuevo[12] = ;
-        informacionUsuarioNuevo[13] = ;
-        informacionUsuarioNuevo[14] = ;
+        informacionUsuarioNuevo[13] = "true";
+        informacionUsuarioNuevo[14] = "false";
 
         /*
             Cambiar cualquier cosa a String:
@@ -42,11 +51,6 @@ public class Interaccion {
         
         try{
             
-            id = interaccionCSV.setIdNuevo();
-            apellido = JOptionPane.showInputDialog("Ingrese apellido de usuario nuevo");
-            segundoNombre = JOptionPane.showInputDialog("Ingrese Segundo Nombre de usuario nuevo");
-            nombre = JOptionPane.showInputDialog("Ingrese nombre de usuario nuevo");
-            telefono = Integer.parseInt(JOptionPane.showInputDialog("Ingrese telefono nuevo"));
             generoString = JOptionPane.showInputDialog("ingrese genero");
             genero = generoString.charAt(0);
             direccion1 = JOptionPane.showInputDialog("Ingrese Direccion 1 nueva");
@@ -55,11 +59,7 @@ public class Interaccion {
             provincia = Integer.parseInt(JOptionPane.showInputDialog("Ingrese provincia nueva"));
             tipoTelefono = Integer.parseInt(JOptionPane.showInputDialog("Ingrese tipo de telefono"));
             correoElectronico = JOptionPane.showInputDialog("Ingrese correo electronico nuevo");;
-            estadoCliente = true;
-            peliculasRentadas = false;
-            
-            nuevoUsuario = {id.valueOf(id), apellido, segundoNombre, nombre, telefono, generoString, genero, direccion1, direccion2};
-            
+
         }catch(Exception e) {
             
             JOptionPane.showMessageDialog(null, "No se pudo crear usuario", "Eror", JOptionPane.ERROR_MESSAGE);
@@ -67,7 +67,7 @@ public class Interaccion {
         }
         
         
-        return vector;
+        return informacionUsuarioNuevo;
         
     }
     
