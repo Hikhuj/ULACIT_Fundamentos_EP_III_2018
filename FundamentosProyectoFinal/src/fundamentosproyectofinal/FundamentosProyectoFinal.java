6a2f64a51b5768269d09aa7b22f7d27d99f42516
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-11-25 19:57:53 -0600

    Nada

--- FundamentosProyectoFinal/src/fundamentosproyectofinal/FundamentosProyectoFinal.java
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/FundamentosProyectoFinal.java
@@ -17,16 +17,7 @@ public class FundamentosProyectoFinal {
     
     public static void main(String[] args) {
 
-        InteraccionConCSV backend = new InteraccionConCSV();
-        Menus menus = new Menus();
         
-        String [] array = {"5", "Demo 5", "Demo 5", "Demo Segundo Nombre 5", "22225050", "m", "Demo 5 Address", "Demo 5 Address 2", "", "", "1", "ema@il", "true", "false"};
-        String [] array2 = {"23", "Demo nombre", "Demo anio", "Demo descripcion", "demoDirector", "1", "0"};
-        
-        
-        // backend.actualizarDatosUsuario(array, "3");
-        // backend.actualizarDatosPelicula(array2, "44444");
-        menus.inicializador();
         
     }
 
