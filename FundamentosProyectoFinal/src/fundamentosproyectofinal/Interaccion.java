a59ea15e3ce535485ef9f43fc7876fbca6b534f0
Róger José Ulate Rivera <roger.p.designe357@gmail.com>
2018-11-25 19:43:41 -0600

    Clase en Interaccion.java limpiada el formato

--- FundamentosProyectoFinal/src/fundamentosproyectofinal/Interaccion.java
+++ FundamentosProyectoFinal/src/fundamentosproyectofinal/Interaccion.java
@@ -31,8 +31,10 @@ public class Interaccion {
             excepcion pues acepta el número como un string, pero esto puede más adelante causar problemas
             porque si se quiere buscar a una persona por ejemplo usando el nombre y el dato contiene un número
             que se ingresó por error, no se va a poder encontrar a esa persona.
-         */
+        */
+        
         String nombre = JOptionPane.showInputDialog("Ingrese el primer nombre del cliente");
+        
         while (nombre.contains("1") || nombre.contains("2")
                 || nombre.contains("3") || nombre.contains("4")
                 || nombre.contains("5") || nombre.contains("6")
@@ -42,15 +44,19 @@ public class Interaccion {
                     + "Ingréselo de nuevo");
             nombre = JOptionPane.showInputDialog("Ingrese el nombre del cliente");
         }
+        
         limpiezaUsuario.setNombre(nombre);
+        
         JOptionPane.showMessageDialog(null, "El nombre ingresado fue: " + limpiezaUsuario.getNombre());
 
     }
 
     public void segundoNombreUsuarioLimpio() {
+        
         //este es un caso similar al de nombre, no se permite guardar el apellido si éste contine números
         String segundoNombre;
         segundoNombre = JOptionPane.showInputDialog("Ingrese el segundo nombre del cliente.");
+        
         while (segundoNombre.contains("1") || segundoNombre.contains("2")
                 || segundoNombre.contains("3") || segundoNombre.contains("4")
                 || segundoNombre.contains("5") || segundoNombre.contains("6")
@@ -60,14 +66,18 @@ public class Interaccion {
                     + "Ingréselo de nuevo");
             segundoNombre = JOptionPane.showInputDialog("Ingrese el segundo nombre del cliente");
         }
+        
         limpiezaUsuario.setSegundoNombre(segundoNombre);
+        
         JOptionPane.showMessageDialog(null, "El segundo nombre ingresado fue: " + limpiezaUsuario.getSegundoNombre());
 
     }
 
     public void apellidoLimpio() {
+        
         String apellido;
         apellido = JOptionPane.showInputDialog("Ingrese el apellido del cliente");
+        
         while (apellido.contains("1") || apellido.contains("2")
                 || apellido.contains("3") || apellido.contains("4")
                 || apellido.contains("5") || apellido.contains("6")
@@ -79,14 +89,19 @@ public class Interaccion {
         }
 
         limpiezaUsuario.setApellido(apellido);
+        
         JOptionPane.showMessageDialog(null, "El apellido ingresado fue: " + limpiezaUsuario.getApellido());
     }
 
     public void tele() {
+        
         boolean done = false;
         int telefono;
+        
         do {
+            
             try {
+                
                 String texto = JOptionPane.showInputDialog("Ingrese el número de teléfono (sin espacios "
                         + " ni guiones)");
                 telefono = Integer.parseInt(texto);
@@ -95,17 +110,22 @@ public class Interaccion {
                 JOptionPane.showMessageDialog(null, "El telefono ingresado es: " + limpiezaUsuario.getTelefono());
 
             } catch (NumberFormatException ex) {
+                
                 JOptionPane.showMessageDialog(null, "Error al ingresar el teléfono, solo escriba números, no letras.");
+            
             }
+            
         } while (!done);
 
     }
 
     public void tipoTelefonoLimpio() {
+        
         String tipoTele;
         int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el"
                 + " número que representa el tipo de teléfono:"
                 + " \n 1. Celular. \n2. Hogar \n 3. Oficina. \n 4. Otro."));
+        
         switch (opcion) {
             case 1:
                 tipoTele = "Celular";
@@ -157,11 +177,13 @@ public class Interaccion {
     }
 
     public void provinciaLimpio() {
+        
         String provincia;
         int opcion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número correspondiente"
                 + " a la ciudad donde vive el cliente:"
                 + " \n 1. San Jose. \n2. Alajuela \n 3. Heredia. \n 4. Cartago. \n 5. Limón."
                 + "\n 6. Puntarenas. \n 7. Guanacaste"));
+        
         switch (opcion) {
             case 1:
                 provincia = "San José";
